18c7b1a4059224b0f251ceea7d9dd7b2f1051f10
Damir <vip.ushkempir@mail.ru>
2025-04-26 12:16:06 +0500

    Base

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        MyBST<Integer> myBST = new MyBST<>();
+        myBST.insert(5);
+        myBST.insert(6);
+        myBST.insert(4);
+        myBST.insert(3);
+        myBST.insert(2);
+        myBST.insert(1);
+
+        myBST.inOrder();
+
+        myBST.delete(2);
+
+        myBST.inOrder();
+
+    }
+}
