b6f3e5c6bb80f06bb1e43037269875183465f518
Damir <vip.ushkempir@mail.ru>
2025-04-28 22:59:03 +0500

    Separation of MyTestingClass and Student from Main

---
+++ src/MyTestingClass.java
@@ -0,0 +1,26 @@
+public class MyTestingClass {
+    private int id;
+    private String name;
+
+    public MyTestingClass(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    // Custom hashCode implementation
+    @Override
+    public int hashCode() {
+        int hash = 17;
+        hash = 31 * hash + id;
+        hash = 31 * hash + (name != null ? name.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        MyTestingClass that = (MyTestingClass) o;
+        return id == that.id && name.equals(that.name);
+    }
+}
